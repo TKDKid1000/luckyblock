0db36c50d1666e4519423871193b89853e71e452
TKDKid1000 <69736165+TKDKid1000@users.noreply.github.com>
2020-11-12 08:43:57 -0800

    Update and rename Main.java to LuckyBlockMain.java

--- src/io/github/tkdkid1000/LuckyBlockMain.java
+++ src/io/github/tkdkid1000/LuckyBlockMain.java
@@ -10,7 +10,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 
-public class Main extends JavaPlugin {
+public class LuckyBlockMain extends JavaPlugin {
     @Override
     public void onEnable() {
     	// enable events and commands
